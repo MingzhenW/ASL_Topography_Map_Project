b1e81c38dddbf9aef34691f6d004d2844054dcd7
Mingzhen Wang <wangmingzhenofficial@gmail.com>
2021-04-15 16:59:08 -0700

    Drawing/Erase function added

--- ASL/Assets/Resources/Script/Player/PlayerDrawRoute.cs
+++ ASL/Assets/Resources/Script/Player/PlayerDrawRoute.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerDrawRoute : MonoBehaviour
 {
     public Camera PlayerCamera;
-    public GameObject MyBrush;
     private float BrushSize = 0.1f;
-    public static List<GameObject> MyBrushList;
+    public static List<GameObject> MyBrushList = new List<GameObject>();
+    public Dropdown MyEraseDropDown;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,6 +39,43 @@ public class PlayerDrawRoute : MonoBehaviour
 
     private static void GetEachBrush(GameObject _myGameObject)
     {
-        //MyBrushList.Add(_myGameObject);
+        MyBrushList.Add(_myGameObject);
+    }
+
+    public void EraseLine()
+    {
+        if (MyEraseDropDown.options[MyEraseDropDown.value].text == "EraseAll")
+        {
+            EraseAllLine();
+        }
+        if (MyEraseDropDown.options[MyEraseDropDown.value].text == "EraseLastTen")
+        {
+            EraseLastTenLine();
+        }
+    }
+
+    private static void EraseAllLine()
+    {
+        foreach (GameObject SingleBrush in MyBrushList)
+        {
+            SingleBrush.GetComponent<ASL.ASLObject>().SendAndSetClaim(() =>
+            {
+                SingleBrush.GetComponent<ASL.ASLObject>().DeleteObject();
+            });
+        }
+
+        MyBrushList.Clear();
+    }
+
+    private static void EraseLastTenLine()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            MyBrushList[MyBrushList.Count - 1].GetComponent<ASL.ASLObject>().SendAndSetClaim(() =>
+            {
+                MyBrushList[MyBrushList.Count - 1].GetComponent<ASL.ASLObject>().DeleteObject();
+            });
+            MyBrushList.RemoveAt(MyBrushList.Count - 1);
+        }
     }
 }
