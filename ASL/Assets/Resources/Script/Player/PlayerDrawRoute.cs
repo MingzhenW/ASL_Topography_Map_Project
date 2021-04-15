93f8c420ba3ab5a18a9cab379206677aa994f9a8
KafuuChino98 <KafuuChino98@users.noreply.github.com>
2021-04-14 21:24:41 -0700

    Light function added, draw function added

--- ASL/Assets/Resources/Script/Player/PlayerDrawRoute.cs
+++ ASL/Assets/Resources/Script/Player/PlayerDrawRoute.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class PlayerDrawRoute : MonoBehaviour
 {
+    public Camera PlayerCamera;
+    public GameObject MyBrush;
+    private float BrushSize = 0.1f;
+    public static List<GameObject> MyBrushList;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        DrawRoute();
+        DrawTheLine();
     }
 
-    private void DrawRoute()
+    private void DrawTheLine()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButton(1))
         {
-
+            Ray MouseRay = PlayerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+            if (Physics.Raycast(MouseRay, out Hit))
+            {
+                if (Hit.collider.tag == "WhiteBoard")
+                {
+                    ASL.ASLHelper.InstantiateASLObject("Brush", Hit.point, Quaternion.identity, "", "", GetEachBrush);
+                }
+            }
         }
+    }
+
+    private static void GetEachBrush(GameObject _myGameObject)
+    {
+        //MyBrushList.Add(_myGameObject);
     }
 }
